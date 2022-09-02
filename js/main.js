c23d529b2b7d37057259e75b6c2d0736c606df0f
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-02 13:11:42 -0700

    Add click event to button that allows grid dimension change

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 
 const container = document.querySelector('.container')
+const getGridChoice = document.querySelector('.howManyBoxes')
 let gridDivs
 let hoveredGrid
+let gridChoice = 16
+
+const editGridChoice = () => {
+    gridChoice = Number(prompt('How many grids per side?'))
+    if(gridChoice > 100){
+        gridChoice = 100
+    }
+    createDivs()
+}
+
+getGridChoice.addEventListener('click', editGridChoice)
+
 
 
 const createDivs = () => {
-    for(i=1; i<=256; i++){
+    for(i=1; i<=gridChoice*gridChoice; i++){
         let newDiv = document.createElement('div')
         newDiv.classList.add(`divGrid`)
         newDiv.style.cssText = "border:solid 1px; border-color:black; width:20px; height:20px;"
