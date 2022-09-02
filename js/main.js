c60a714e2b8cd5d22686cb8a43d202a26a1e7ce7
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-02 11:47:06 -0700

    Add loop to add divs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 
 const div = document.createElement('div')
-div.style.cssText = 'width:16px; height:16px;'
+const container = document.querySelector('.container')
+div.style.cssText = "background-color:red; width:15px; height:15px;"
+
+
+
+const createDivs = () => {
+    for(i=1; i<=32; i++){
+        let newDiv = document.createElement('div')
+        newDiv.classList.add(`div${i}`)
+        container.appendChild(newDiv)
+    }
+}
+
+createDivs()
