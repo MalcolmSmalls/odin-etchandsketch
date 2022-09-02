f8e5ab04a580edd972d79a630eb7cf475153c09b
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-02 12:33:40 -0700

    Finish hovering effect on grid

--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
 
-const div = document.createElement('div')
 const container = document.querySelector('.container')
-div.style.cssText = "background-color:red; width:15px; height:15px;"
-
+let gridDivs
+let hoveredGrid
 
 
 const createDivs = () => {
     for(i=1; i<=256; i++){
         let newDiv = document.createElement('div')
-        newDiv.classList.add(`div${i}`)
+        newDiv.classList.add(`divGrid`)
         newDiv.style.cssText = "border:solid 1px; border-color:black; width:20px; height:20px;"
         container.appendChild(newDiv)
+        gridDivs = document.querySelectorAll('.divGrid')
     }
 }
 
 createDivs()
+
+
+// const addColor = () => {
+//     console.log('hello');
+//     gridDivs.style.cssText = "border:solid 1px; border-color:black; width:20px; height:20px;background-color:black;"
+// }
+
+gridDivs.forEach((grid, i) => {
+    grid.addEventListener('mouseover', function(){
+        hoveredGrid = grid
+        hoveredGrid.style.cssText = "border:solid 1px; border-color:black; width:20px; height:20px;background-color:black;"
+    }
+)})
+
+// let divGridVar = document.querySelector('.divGrid')
+
+
+
+
+
+// divGridVar.addEventListener('mouseover', addColor, false)
+
