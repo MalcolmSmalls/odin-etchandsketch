387d402e8b47792fc15185fd78a05e06c179d0fa
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-02 13:52:31 -0700

    Allow for dimensions to be changed and still sketch, removal of old grid

--- js/main.js
+++ js/main.js
@@ -6,8 +6,20 @@ let hoveredGrid
 let gridChoice = 16
 let divSize = document.querySelector('.container').clientWidth / gridChoice
 
+const etchAndSketch = () => {
+    gridDivs.forEach((grid, i) => {
+        grid.addEventListener('mouseover', function(){
+            hoveredGrid = grid
+            hoveredGrid.style.cssText = `border:solid 1px; border-color:black; width:${divSize}px; height:${divSize}px; background-color:black;`    
+        })
+    })
+}
+
 const editGridChoice = () => {
     gridChoice = Number(prompt('How many grids per side?'))
+    for(i=0; i< gridDivs.length;i++){
+        gridDivs[i].remove()
+    }
     if(gridChoice > 100){
         gridChoice = 100
     }
@@ -27,18 +39,13 @@ const createDivs = () => {
         container.appendChild(newDiv)
         gridDivs = document.querySelectorAll('.divGrid')
     }
+    etchAndSketch()
 }
 
 createDivs()
 
 
 
-gridDivs.forEach((grid, i) => {
-    grid.addEventListener('mouseover', function(){
-        hoveredGrid = grid
-        hoveredGrid.style.cssText = `border:solid 1px; border-color:black; width:${divSize}px; height:${divSize}px; background-color:black;`    
-    })
-})
 
 
 
