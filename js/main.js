980e4749943df94207d1f36459ab8fbfa27248f7
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-02 11:54:41 -0700

    Add CSS and style to grid

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const createDivs = () => {
     for(i=1; i<=32; i++){
         let newDiv = document.createElement('div')
         newDiv.classList.add(`div${i}`)
+        newDiv.style.cssText = "border:solid 1px; border-color:black; width:15px; height:15px;"
         container.appendChild(newDiv)
     }
 }
