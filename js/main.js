6e564ce5a624fcadc99de696283f0f8f038583ce
Malcolm Smalls <smallsbeats@gmail.com>
2022-09-02 11:59:09 -0700

    Add rest of boxes, fix width so boxes stack 16x16

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ div.style.cssText = "background-color:red; width:15px; height:15px;"
 
 
 const createDivs = () => {
-    for(i=1; i<=32; i++){
+    for(i=1; i<=256; i++){
         let newDiv = document.createElement('div')
         newDiv.classList.add(`div${i}`)
-        newDiv.style.cssText = "border:solid 1px; border-color:black; width:15px; height:15px;"
+        newDiv.style.cssText = "border:solid 1px; border-color:black; width:20px; height:20px;"
         container.appendChild(newDiv)
     }
 }
